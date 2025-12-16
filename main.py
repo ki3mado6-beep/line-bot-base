f8e42bd00d036325ff86f2706629f1ac9c39ab7b
ki3mado6-beep <ki3mado6@gmail.com>
2025-12-16 22:44:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "FastAPI on Cloud Run is running"}
+
+from fastapi import Request
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    body = await request.body()
+    print(body)
+    return "OK"
